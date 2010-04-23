83ad8396b6ba89fd850b2f0b7a5dd45dbc7beb61
Jakub Lipinski <jakub.lipinski@gmail.com>
2010-04-23 17:08:15 +0200

    initial maximized form

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using pl.polidea.lab.Web_Page_Screensaver;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Application.Run(new ScreensaverForm());
+        }
+    }
+}
---
+++ ScreensaverForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    public partial class ScreensaverForm : Form
+    {
+        public ScreensaverForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
