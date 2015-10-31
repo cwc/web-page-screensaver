bd82c7b3c3c687d24abfac8050151b66af8f2414
Cameron Currie <me@cameroncurrie.net>
2015-10-30 19:35:28 -0500

    Force a modern version of IE for rendering page

--- Program.cs
+++ Program.cs
@@ -1,25 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using pl.polidea.lab.Web_Page_Screensaver;
+using Microsoft.Win32;
+using System.IO;
 
 namespace pl.polidea.lab.Web_Page_Screensaver
 {
     static class Program
     {
-        public static readonly string KEY = "Software\\Web-Page-Screensaver";  
+        public static readonly string KEY = "Software\\Web-Page-Screensaver";
 
         [STAThread]
         static void Main(string[] args)
         {
+            // Set version of embedded browser (http://weblog.west-wind.com/posts/2011/May/21/Web-Browser-Control-Specifying-the-IE-Version)
+            var exeName = Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", exeName, 0x2AF8, RegistryValueKind.DWord);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             if (args.Length > 0 && args[0].ToLower().Contains("/p"))
                 return;
 
-            if(args.Length > 0 && args[0].ToLower().Contains("/c"))
+            if (args.Length > 0 && args[0].ToLower().Contains("/c"))
                 Application.Run(new PreferencesForm());
             else
                 Application.Run(new ScreensaverForm());
