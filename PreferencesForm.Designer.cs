da1c0f3397f94a9a7dde01149cf06320a6525f14
Cameron Currie <me@cameroncurrie.net>
2015-11-03 18:08:18 -0600

    Use listbox for adding/removing site URLs
    Also set project to target .NET 4.6

--- PreferencesForm.Designer.cs
+++ PreferencesForm.Designer.cs
@@ -31,19 +31,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.llProjectLocationUrl = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
-            this.tbUrl = new System.Windows.Forms.TextBox();
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.cbCloseOnActivity = new System.Windows.Forms.CheckBox();
             this.nudRotationInterval = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
+            this.lbUrls = new System.Windows.Forms.ListBox();
+            this.tbUrlToAdd = new System.Windows.Forms.TextBox();
+            this.addUrlButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nudRotationInterval)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 9);
+            this.label1.Location = new System.Drawing.Point(76, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +54,7 @@
             // llProjectLocationUrl
             // 
             this.llProjectLocationUrl.AutoSize = true;
-            this.llProjectLocationUrl.Location = new System.Drawing.Point(21, 25);
+            this.llProjectLocationUrl.Location = new System.Drawing.Point(22, 25);
             this.llProjectLocationUrl.Name = "llProjectLocationUrl";
             this.llProjectLocationUrl.Size = new System.Drawing.Size(233, 13);
             this.llProjectLocationUrl.TabIndex = 1;
@@ -63,23 +65,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Location = new System.Drawing.Point(12, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Website URLs (space-separated):";
-            // 
-            // tbUrl
-            // 
-            this.tbUrl.Location = new System.Drawing.Point(15, 68);
-            this.tbUrl.Name = "tbUrl";
-            this.tbUrl.Size = new System.Drawing.Size(246, 20);
-            this.tbUrl.TabIndex = 1;
+            this.label2.Text = "Website URLs";
             // 
             // okButton
             // 
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(105, 159);
+            this.okButton.Location = new System.Drawing.Point(102, 302);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 4;
@@ -90,7 +85,7 @@
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(186, 159);
+            this.cancelButton.Location = new System.Drawing.Point(183, 302);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 5;
@@ -103,17 +98,17 @@
             this.cbCloseOnActivity.AutoSize = true;
             this.cbCloseOnActivity.Checked = true;
             this.cbCloseOnActivity.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbCloseOnActivity.Location = new System.Drawing.Point(15, 120);
+            this.cbCloseOnActivity.Location = new System.Drawing.Point(15, 258);
             this.cbCloseOnActivity.Name = "cbCloseOnActivity";
             this.cbCloseOnActivity.Size = new System.Drawing.Size(153, 17);
             this.cbCloseOnActivity.TabIndex = 6;
             this.cbCloseOnActivity.Text = "Close on mouse movement";
             this.cbCloseOnActivity.UseVisualStyleBackColor = true;
             // 
-            // rotationInterval
+            // nudRotationInterval
             // 
-            this.nudRotationInterval.Location = new System.Drawing.Point(15, 94);
-            this.nudRotationInterval.Name = "rotationInterval";
+            this.nudRotationInterval.Location = new System.Drawing.Point(15, 232);
+            this.nudRotationInterval.Name = "nudRotationInterval";
             this.nudRotationInterval.Size = new System.Drawing.Size(40, 20);
             this.nudRotationInterval.TabIndex = 7;
             this.nudRotationInterval.Value = new decimal(new int[] {
@@ -125,11 +120,36 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(61, 96);
+            this.label3.Location = new System.Drawing.Point(61, 234);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Seconds to display each site";
+            // 
+            // lbUrls
+            // 
+            this.lbUrls.FormattingEnabled = true;
+            this.lbUrls.Location = new System.Drawing.Point(15, 72);
+            this.lbUrls.Name = "lbUrls";
+            this.lbUrls.Size = new System.Drawing.Size(246, 121);
+            this.lbUrls.TabIndex = 9;
+            // 
+            // tbUrlToAdd
+            // 
+            this.tbUrlToAdd.Location = new System.Drawing.Point(15, 201);
+            this.tbUrlToAdd.Name = "tbUrlToAdd";
+            this.tbUrlToAdd.Size = new System.Drawing.Size(181, 20);
+            this.tbUrlToAdd.TabIndex = 10;
+            // 
+            // addUrlButton
+            // 
+            this.addUrlButton.Location = new System.Drawing.Point(203, 199);
+            this.addUrlButton.Name = "addUrlButton";
+            this.addUrlButton.Size = new System.Drawing.Size(58, 23);
+            this.addUrlButton.TabIndex = 11;
+            this.addUrlButton.Text = "Add";
+            this.addUrlButton.UseVisualStyleBackColor = true;
+            this.addUrlButton.Click += new System.EventHandler(this.addUrlButton_Click);
             // 
             // PreferencesForm
             // 
@@ -137,13 +157,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(273, 194);
+            this.ClientSize = new System.Drawing.Size(277, 332);
+            this.Controls.Add(this.addUrlButton);
+            this.Controls.Add(this.tbUrlToAdd);
+            this.Controls.Add(this.lbUrls);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.nudRotationInterval);
             this.Controls.Add(this.cbCloseOnActivity);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
-            this.Controls.Add(this.tbUrl);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.llProjectLocationUrl);
             this.Controls.Add(this.label1);
@@ -165,11 +187,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel llProjectLocationUrl;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbUrl;
         private System.Windows.Forms.Button okButton;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.CheckBox cbCloseOnActivity;
         private System.Windows.Forms.NumericUpDown nudRotationInterval;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbUrls;
+        private System.Windows.Forms.TextBox tbUrlToAdd;
+        private System.Windows.Forms.Button addUrlButton;
     }
 }
--- PreferencesForm.cs
+++ PreferencesForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using Microsoft.Win32;
-using System.Text;
 using System.Windows.Forms;
 
 namespace pl.polidea.lab.Web_Page_Screensaver
@@ -19,19 +14,63 @@ namespace pl.polidea.lab.Web_Page_Screensaver
         public const string URL_PREF_DEFAULT = "https://www.google.com/trends/hottrends/visualize?nrow=5&ncol=5 https://screensaver.twingly.com/ http://map.ipviking.com/";
         public const string CLOSE_ON_ACTIVITY_PREF_DEFAULT = "True";
         public const string INTERVAL_PREF_DEFAULT = "30";
+        private ContextMenuStrip urlsContextMenu;
 
         public PreferencesForm()
         {
             InitializeComponent();
+
+            urlsContextMenu = new ContextMenuStrip();
+            urlsContextMenu.Opening += UrlsContextMenu_Opening;
+
+            var removeUrl = urlsContextMenu.Items.Add("Remove URL");
+            removeUrl.Click += RemoveUrl_Click;
+
+            lbUrls.ContextMenuStrip = urlsContextMenu;
+            lbUrls.MouseDown += LbUrls_MouseDown;
+        }
+
+        private void UrlsContextMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (lbUrls.SelectedItem == null)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void LbUrls_MouseDown(object sender, MouseEventArgs e)
+        {
+            var clickedIndex = lbUrls.IndexFromPoint(e.Location);
+            lbUrls.SelectedIndex = lbUrls.IndexFromPoint(e.Location);
+        }
+
+        private void RemoveUrl_Click(object sender, EventArgs e)
+        {
+            if (lbUrls.SelectedItem != null)
+            {
+                lbUrls.Items.Remove(lbUrls.SelectedItem);
+            }
         }
 
         private void PreferencesForm_Load(object sender, EventArgs e)
         {
             RegistryKey reg = Registry.CurrentUser.CreateSubKey(Program.KEY);
-            tbUrl.Text = (string)reg.GetValue(URL_PREF, URL_PREF_DEFAULT);
+            loadUrls(reg);
             cbCloseOnActivity.Checked = Boolean.Parse((string)reg.GetValue(CLOSE_ON_ACTIVITY_PREF, CLOSE_ON_ACTIVITY_PREF_DEFAULT));
             nudRotationInterval.Value = int.Parse((string)reg.GetValue(INTERVAL_PREF, INTERVAL_PREF_DEFAULT));
             reg.Close();
+        }
+
+        private void loadUrls(RegistryKey reg)
+        {
+            lbUrls.Items.Clear();
+
+            var urls = ((string)reg.GetValue(URL_PREF, URL_PREF_DEFAULT)).Split(' ');
+            
+            foreach (var url in urls)
+            {
+                lbUrls.Items.Add(url);
+            }
         }
 
         protected override void OnClosed(EventArgs e)
@@ -39,13 +78,20 @@ namespace pl.polidea.lab.Web_Page_Screensaver
             if (DialogResult == DialogResult.OK)
             {
                 RegistryKey reg = Registry.CurrentUser.CreateSubKey(Program.KEY);
-                reg.SetValue(URL_PREF, tbUrl.Text);
+                saveUrls(reg);
                 reg.SetValue(CLOSE_ON_ACTIVITY_PREF, cbCloseOnActivity.Checked);
                 reg.SetValue(INTERVAL_PREF, nudRotationInterval.Value);
                 reg.Close();
             }
 
             base.OnClosed(e);
+        }
+
+        private void saveUrls(RegistryKey reg)
+        {
+            var urls = String.Join(" ", lbUrls.Items.Cast<String>());
+                       
+            reg.SetValue(URL_PREF, urls);
         }
 
         private void llProjectLocationUrl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -63,5 +109,11 @@ namespace pl.polidea.lab.Web_Page_Screensaver
             Close();
         }
 
+        private void addUrlButton_Click(object sender, EventArgs e)
+        {
+            var url = tbUrlToAdd.Text;
+
+            lbUrls.Items.Add(url);
+        }
     }
 }
